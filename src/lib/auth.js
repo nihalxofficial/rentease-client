71c72a0c8a081997a3ecb251861c260d90868f2c
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-06-24 09:01:53 +0600

    JWT setup

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db("rentease_db");
@@ -28,4 +29,7 @@ export const auth = betterAuth({
       },
     },
   },
+  plugins: [
+        jwt(), 
+    ]
 });
--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,16 +1,30 @@
+import { getToken } from "./session";
+
 const Api = process.env.NEXT_PUBLIC_API_URL
 
-export const serverFetch = async(path)=>{
-    const res = await fetch(`${Api}${path}`)
+export const serverFetch = async (path, requireAuth = false) => {
+    const headers = {};
+    
+    if (requireAuth) {
+        const token = await getToken();
+        headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    const res = await fetch(`${Api}${path}`, {
+        cache: 'no-store',
+        headers
+    });
     const data = await res.json();
     return data;
-}
+};
 
 export const serverMutation = async(path, data, method="POST")=>{
-    const res = await fetch(`${Api}${path}`, {
+    const token = await getToken()
+    const res = await fetch(`${Api}${path}`,{
         method: method,
         headers: {
             "Content-Type" : "application/json",
+            Authorization : `Bearer ${token}`
         },
         body: JSON.stringify(data)
     })
